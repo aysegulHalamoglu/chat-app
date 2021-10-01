d8e758a9db3a8ada8b7e7119d2e1cdd1b9c6e408
Aysegul H <ahalamoglu19@gmail.com>
2021-10-02 02:13:23 +0300

    [UPDATE] UserHeader: if there is selectedUser display the fullname

--- my-chat-app/src/pages/Chat/components/UserHeader.js
+++ my-chat-app/src/pages/Chat/components/UserHeader.js
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import { LoginContext } from "../../../context/LoginContext";
 
 export default function UserHeader() {
-  const {} = useContext(LoginContext);
+  const { selectedUser } = useContext(LoginContext);
   return (
     <div className="userHeader">
       <div className="userImage">Image</div>
-      <div className="userName"> John Doe</div>
+      <div className="userName">
+        {`${selectedUser?.first_name} ${selectedUser?.last_name}`}
+      </div>
       <div className="more">...</div>
     </div>
   );
