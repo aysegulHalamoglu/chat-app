6a04623f609c4a7490a9613c40fdf41959f03679
Aysegul H <ahalamoglu19@gmail.com>
2021-10-02 00:28:41 +0300

    [UPDATE]  FriendList: friends iteration over map function

--- my-chat-app/src/pages/Chat/components/FriendList.js
+++ my-chat-app/src/pages/Chat/components/FriendList.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LoginContext } from "../../../context/LoginContext";
 
 export default function FriendList() {
-  return <div className="friendList"></div>;
+  const { friendList } = useContext(LoginContext);
+  return (
+    <div className="friendList">
+      {friendList.map((item) => {
+        return <div>{`${item.first_name} ${item.last_name}`} </div>;
+      })}
+    </div>
+  );
 }
