211a5b80212d862d1aea933f04a1584b91ad8a07
Aysegul H <ahalamoglu19@gmail.com>
2021-10-04 00:51:11 +0300

    [UPDATE] cleaning extras

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,7 +1,4 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
 const CustomButton = (props) => {
-  const { theming } = useContext(ThemeContext);
   const { text, onClick, className, style } = props;
   return (
     <button style={style} className={className} onClick={onClick}>
